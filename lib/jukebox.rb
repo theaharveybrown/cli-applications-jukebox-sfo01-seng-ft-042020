82dc73f4f06254483edae191cc44e257c9ccbd2c
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-04-14 19:19:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,6 +29,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_song = gets.strip
   if (1..9).include?(user_song.to_i)
+    user_song = user_song.to_i
     puts "Playing #{songs[user_song-1]}"
   else 
     input_type = user_song.is_a?(Integer)
