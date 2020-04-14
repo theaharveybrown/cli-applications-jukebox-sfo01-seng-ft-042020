677c0acdd9111e14c559eff9861427312515787a
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-04-14 19:10:04 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,7 +28,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_song = gets.strip
-  if list(songs).include?(user_song)
+  
     puts "Playing #{user_song}"
   else 
     puts "Invalid input, please try again"
